b698ab251c2d2a0c8bfd046197e535c8ee8a8f0d
노규선 <kusun1020@gmail.com>
2023-03-10 22:11:15 +0900

    요청사항 수정했습니다.

--- Calculator.java
+++ Calculator.java
@@ -13,6 +13,11 @@ class Calculate {
         //	더하기
     }
 
+    /**
+     * @param n1
+     * @param n2
+     * @return n1과 n2에 대해서 뺴기를 수행하는 함수입니다.
+     */
     static int sub(int n1, int n2) {
         return n1 - n2;
     }
